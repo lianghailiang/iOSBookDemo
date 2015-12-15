9acfa5fa12cd7f231b4c2a2529ad0998965d4005
lianghailiang <lianghailiang@jinvovo.com>
2015-12-15 11:04:02 +0800

    Initial Commit

---
+++ iOSBookDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSBookDemo
+//
+//  Created by lianghailiang on 15/12/15.
+//  Copyright © 2015年 jinvovo_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSBookDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSBookDemo
+//
+//  Created by lianghailiang on 15/12/15.
+//  Copyright © 2015年 jinvovo_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
