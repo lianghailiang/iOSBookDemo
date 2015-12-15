9a6fec57da7b20a8605efcd23064202d1fa604a4
lianghailiang <lianghailiang@jinvovo.com>
2015-12-15 18:06:50 +0800

    nslogyes

--- iOSBookDemo/ViewController.m
+++ iOSBookDemo/ViewController.m
@@ -17,11 +17,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [UIFont familyNames];//字体库
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *document = paths.firstObject;
+
+    __block int a  = 0;
+    void (^sillyBlock)(void  ) = ^{ a = 47 ;};
+    
+    NSLog(@"a == %d",a);
+    
+    sillyBlock();
+    NSLog(@"a = = %d",a);
+    
+    NSLog(@"YES");
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)doWork:(UIButton *)sender {
+    NSDate *statTime = [NSDate date];
+    NSLog(@"%@",statTime);
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(queue, ^{
+        NSLog(@"jjj");
+        [NSThread sleepForTimeInterval:3];//挂起3秒，不能执行其他操作
+        NSLog(@"kkk");
+        [NSThread sleepForTimeInterval:2];
+        NSLog(@"lll");
+        
+        NSDate *statTime = [NSDate date];
+        NSLog(@"%@",statTime);
+
+    });
+    
+    
+    
 }
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"1111111111111");
+    
+    
+}
+
 
 @end
